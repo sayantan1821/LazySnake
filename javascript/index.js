d7cf0cb1b8260bc82e663b2108f627edb2cbb64f
Sayantan Kapat <ksayantan1823@gmail.com>
2021-05-12 23:58:40 +0530

    adding game sound

--- javascript/index.js
+++ javascript/index.js
@@ -28,9 +28,12 @@ function gameEngine () {
         snakeElement.style.gridRowStart = e.y;
         snakeElement.style.gridColumnStart = e.x;
         snakeElement.classList.add('snake');
-        if(index == 0) {
+        if(index === 0) {
             snakeElement.classList.add('head');
+        } else {
+            snakeElement.classList.add('snake');
         }
+        
         board.appendChild(snakeElement);
     });
     foodElement = document.createElement('div');
@@ -41,3 +44,23 @@ function gameEngine () {
 }
 
 window.requestAnimationFrame(main);
+window.addEventListener('keydown', e => {
+    inputDir = {x: 0, y: 1}
+    moveSound.play();
+    switch (e.key) {
+        case "ArrowUp" :
+            console.log("ArrowUp");
+            break;
+        case "ArrowDown" :
+            console.log("ArrowDown");
+            break;
+        case "ArrowLeft" :
+            console.log("ArrowLeft");
+            break;
+        case "ArrowRight" :
+            console.log("ArrowRight");
+            break;
+        default:
+            break;
+    }
+})
