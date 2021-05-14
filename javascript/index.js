c8b5b3a4355aa76785949692c7f0db6cddbd9dd0
Sayantan Kapat <ksayantan1823@gmail.com>
2021-05-15 03:27:18 +0530

    making hard

--- javascript/index.js
+++ javascript/index.js
@@ -3,8 +3,11 @@ const foodSound = new Audio('../music/food.mp3');
 const gameOverSound = new Audio('../music/gameover.mp3');
 const moveSound = new Audio('../music/move.mp3');
 const musicSound = new Audio("../music/music.mp3");
+musicSound.volume = 0.2;
 
 let speed = prompt("Please your snake's speed: ",7);
+let userSpeed = speed;
+
 let lastPrint = 0
 let snakeArr = [
     {x: 13, y: 15}
@@ -14,7 +17,8 @@ let food = {
     y: 7
 };
 let score = 0;
-musicSound.play();
+let subspeed = 0;
+
 function main(ctime) {
     window.requestAnimationFrame(main);
     if((ctime - lastPrint)/1000 < 1/speed){
@@ -43,10 +47,17 @@ function gameEngine () {
         snakeArr = [{x: 13, y: 15}];
         musicSound.play();
         score = 0;
+        speed = userSpeed;
+        subspeed = 0;
     }
     if(snakeArr[0].y == food.y && snakeArr[0].x == food.x) {
         foodSound.play();
         score++;
+        subspeed++;
+        if(subspeed == 5) {
+            speed++;
+            subspeed = 0;
+        }
         scoreBox.innerHTML = "Score : " + score;
         speedBox.innerHTML = "Speed : " + speed;
         if(score > hiscoreval) {
@@ -88,7 +99,7 @@ function gameEngine () {
     foodElement.classList.add('food');
     board.appendChild(foodElement);
 }
-
+musicSound.play();
 let hiscore = localStorage.getItem('hiscore');
 if(hiscore === null) {
     hiscoreval = 0;
